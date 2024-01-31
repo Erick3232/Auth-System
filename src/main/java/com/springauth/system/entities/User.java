722de04ab863bedb554db67ddb857c1eeb2ce320
Erick <erick_miyagi34231@hotmail.com>
2024-01-31 14:27:51 -0300

    create user

--- src/main/java/com/springauth/system/entities/User.java
+++ src/main/java/com/springauth/system/entities/User.java
@@ -1,5 +1,26 @@
 package com.springauth.system.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name = "users")
+@Table(name = "new_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class User {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password;
 }
