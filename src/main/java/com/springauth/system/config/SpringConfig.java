bb3377c3decc721662fd22975b308ad034bb64fb
leonardolermen <leosamer.lermen@gmail.com>
2024-02-10 17:24:35 -0300

    add perm login/register no spring config

--- src/main/java/com/springauth/system/config/SpringConfig.java
+++ src/main/java/com/springauth/system/config/SpringConfig.java
@@ -2,6 +2,7 @@ package com.springauth.system.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -21,7 +22,13 @@ public class SpringConfig {
         return httpSecurity.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                .anyRequest().authenticated()
+                )
                 .build();
+                
             }
     
     @Bean
