ddfa4950a433ef8a316aa9c705abf379c3b5e7a3
leonardolermen <leosamer.lermen@gmail.com>
2024-02-12 22:08:33 -0300

    iniciando a criação do jwt token

--- src/main/java/com/springauth/system/controller/AuthController.java
+++ src/main/java/com/springauth/system/controller/AuthController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.springauth.system.DTOs.AuthenticationDTO;
 import com.springauth.system.DTOs.RegisterDTO;
 import com.springauth.system.entities.User;
