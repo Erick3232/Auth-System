ed7564816f198dae5db5d9d6c322757ab32c9c13
leonardolermen <leosamer.lermen@gmail.com>
2024-02-13 00:34:44 -0300

    transaction and transactionServices devidamente criados

--- src/main/java/com/springauth/system/services/AcountService.java
+++ src/main/java/com/springauth/system/services/AcountService.java
@@ -3,13 +3,14 @@ package com.springauth.system.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.springauth.system.entities.User;
 import com.springauth.system.exceptions.ResourceNotFoundException;
 import com.springauth.system.repositories.UserRepository;
 
 
-
+@Service
 public class AcountService {
 
     @Autowired
