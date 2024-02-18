17c3b031e28adcd2af9699dcfae4e6a62cca1788
leonardolermen <leosamer.lermen@gmail.com>
2024-02-18 18:04:41 -0300

    register page

--- src/main/java/com/springauth/system/config/SpringConfig.java
+++ src/main/java/com/springauth/system/config/SpringConfig.java
@@ -24,7 +24,6 @@ public class SpringConfig{
                 .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                 .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                .requestMatchers(HttpMethod.GET, "/swagger-ui.html").permitAll()
                 .anyRequest().permitAll()
                 )
                 .build();
--- src/main/resources/templates/register/app.js
+++ src/main/resources/templates/register/app.js
@@ -7,7 +7,6 @@ document.getElementById('registration-form').addEventListener('submit', function
     const password = document.getElementById('password').value;
     const personType = document.getElementById('gender').value;
     const document = document.getElementById('username').value;
-    const phoneNumber = document.getElementById('phone-number').value;
     const confirmPassword = document.getElementById('confirm-password').value;
 
     // Perform validation and further processing here
@@ -15,9 +14,8 @@ document.getElementById('registration-form').addEventListener('submit', function
         fullName,
         email,
         password,
-        gender,
+        personType,
         username,
-        phoneNumber,
         confirmPassword
     });
 });
