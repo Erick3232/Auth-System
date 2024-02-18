d645ed82b103618ecd6b38538c30468ccf7cf144
Erick <erick_miyagi34231@hotmail.com>
2024-02-18 19:41:34 -0300

    fix style

--- src/main/java/com/springauth/system/controller/HomeController.java
+++ src/main/java/com/springauth/system/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.springauth.system.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
+
+    @GetMapping("/")
     public ModelAndView index(){
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("home/index");
+        mv.setViewName("register");
         return mv;
     }
 }
