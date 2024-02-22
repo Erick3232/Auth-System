41b7299ec779c81a6001786e4f5c73e347b7da17
Erick <erick_miyagi34231@hotmail.com>
2024-02-22 16:41:50 -0300

    feat: add service,cards,support html pages and connection with dashboard

--- src/main/java/com/springauth/system/controller/WalletController.java
+++ src/main/java/com/springauth/system/controller/WalletController.java
@@ -29,9 +29,27 @@ public class WalletController {
     private AcountService acountService;
 
     @GetMapping("/dashboard")
-    public ModelAndView index(){
+    public ModelAndView dashboard(){
         ModelAndView mv = new ModelAndView();
         mv.setViewName("dashboard");
+        return mv;
+    }
+    @GetMapping("/dashboard/cards")
+    public ModelAndView cards(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("cards");
+        return mv;
+    }
+    @GetMapping("/dashboard/services")
+    public ModelAndView services(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("services");
+        return mv;
+    }
+    @GetMapping("/dashboard/support")
+    public ModelAndView support(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("support");
         return mv;
     }
 
