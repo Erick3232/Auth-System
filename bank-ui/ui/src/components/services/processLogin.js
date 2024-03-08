22a69d3ebe39e7074784021c13d5be76489810d4
leonardolermen <leosamer.lermen@gmail.com>
2024-03-08 15:33:04 -0300

    add axios

---
+++ bank-ui/ui/src/components/services/processLogin.js
@@ -0,0 +1,17 @@
+// api.js
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:8000', // url
+});
+
+export const createUser = async (userData) => {
+  try {
+    const response = await api.post('/auth/process', userData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default api;
