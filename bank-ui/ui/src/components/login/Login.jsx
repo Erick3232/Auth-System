8f67d253a3782b9af13c31c5c870a086353039ea
Erick <erick_miyagi34231@hotmail.com>
2024-03-12 16:20:32 -0300

    feat(layout): add validation login in API

--- bank-ui/ui/src/components/login/Login.jsx
+++ bank-ui/ui/src/components/login/Login.jsx
@@ -1,21 +1,57 @@
-import React,{useRef} from 'react';
+import React,{useRef, useState} from 'react';
 import './Login.css';
 import { FaLock } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import Register from '../register/Register'; 
-import { BrowserRouter as Router, Route,Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route,Routes, Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Login = () => {
+
+    const [values, setValues] = useState({
+        login: '',
+        password: ''
+    })
+    const history = useNavigate();
+    const[mensagem, setMensagens] = useState('')
+
+    const handleInputChange = (event) => {
+        event.preventDefault();
+        const { name, value } = event.target;
+        setValues((prevValues) => ({
+            ...prevValues,
+            [name]: value
+        }));
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const {login, password} = values;
+
+        if(login && password){
+            try{
+                const response = await axios.post('http://localhost:8080/auth/processLogin', {
+                    login: login,
+                    password: password
+                });
+                console.log(response)
+                setMensagens('Entrada bem-sucedida!')
+                history('/dashboard');
+            }catch(error){
+                setMensagens("Usuário ou Senha inválida")
+                console.error("Erro: ", error)
+            }
+        }
+    }
   return (
     <div className="wrapper">
-        <form action=''>
+        <form onSubmit={handleSubmit}>
             <h1>Login</h1>
             <div className="input-boxx">
-                <input type='text' placeholder='Email' required />
+                <input type='text' placeholder='Username' value={values.login} name='login' onChange={handleInputChange} required />
                 <MdEmail className='icon-name' />
             </div>
             <div className="input-boxx">
-                <input type='password' placeholder='Password' required />
+                <input type='password' value={values.password} placeholder='Password' name='password' onChange={handleInputChange} required />
                 <FaLock className='icon-password' />
             </div>
 
