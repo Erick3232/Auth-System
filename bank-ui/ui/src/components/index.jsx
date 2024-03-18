91e7ba055285ba289589c73650b23a2e390a200f
leonardolermen <leosamer.lermen@gmail.com>
2024-03-18 11:42:29 -0300

    new dashboard ui

--- bank-ui/ui/src/components/index.jsx
+++
@@ -1,7 +0,0 @@
-import { Text } from "./Text";
-import { Heading } from "./Heading";
-import { Input } from "./Input";
-import { Img } from "./Img";
-import { Button } from "./Button";
-import { ReactTable } from "./ReactTable";
-export { Text, Heading, Input, Img, Button, ReactTable };
