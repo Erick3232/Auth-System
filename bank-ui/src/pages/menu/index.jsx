eb571808f9d513216049e55ac0a6a6a914d90335
Erick <erick_miyagi34231@hotmail.com>
2024-03-22 11:44:51 -0300

    feat(layout): add some components at dashboard

--- bank-ui/src/pages/menu/index.jsx
+++ bank-ui/src/pages/menu/index.jsx
@@ -1,5 +1,11 @@
 import { Link } from 'react-router-dom'
 import './Menu.css'
+import { MdAccountCircle } from "react-icons/md";
+import { BiTransfer } from "react-icons/bi";
+import { FaMoneyCheckAlt } from "react-icons/fa";
+import { RiMoneyDollarBoxLine } from "react-icons/ri";
+import { TbPigMoney } from "react-icons/tb";
+import { HiArrowSmRight } from "react-icons/hi";
 
 const Menu = () => {
     return(
@@ -22,6 +28,96 @@ const Menu = () => {
                 </div>
             </div>
             </header>
+            <div className="app-body">
+                <div className="app-body-navigation">
+                    <nav className="navigation">
+                        <button className="user-profile">
+                            <span>
+                            </span>
+                        </button>
+                        <Link to="/menu/account" className='menu'>
+                        <MdAccountCircle className='icons-menu'></MdAccountCircle>
+                        <span>Account</span>
+
+                        </Link>
+                        <Link to="/menu/account" className='menu'>
+                        <MdAccountCircle className='icons-menu'></MdAccountCircle>
+                        <span>Dashboard</span>
+
+                        </Link>
+                        <Link to="/menu/account" className='menu'>
+                        <MdAccountCircle className='icons-menu'></MdAccountCircle>
+                        <span>History</span>
+
+                        </Link>
+                        <Link to="/menu/account" className='menu'>
+                        <MdAccountCircle className='icons-menu'></MdAccountCircle>
+                        <span>Exchange</span>
+                        </Link>
+                    </nav>
+                    <footer className='footer'>
+                        <h1>Auth Bank <small>©</small></h1>
+                        <div>
+                            Leonardo & Erick © <br/>
+                            All Rights Reserved 2024
+                        </div>
+                    </footer>
+                </div>
+                <div className="app-body-main-content">
+                    <section className="service-section">
+                        <span id='userBalance'></span>
+                        <div className="tiles">
+                            <article className="tile">
+                                <div className="tile-header">
+                                    <BiTransfer></BiTransfer>
+                                    <h3>
+                                        <span>Transfers</span>
+                                    </h3>
+                                </div>
+                                <Link to="/menu/services" className='services'>
+                                    <span>Got to services</span>
+                                    <HiArrowSmRight className='icon-button'></HiArrowSmRight>
+                                </Link>
+                            </article>
+                            <article className="tile">
+                                <div className="tile-header">
+                                    <RiMoneyDollarBoxLine></RiMoneyDollarBoxLine>
+                                    <h3>
+                                        <span>Deposit</span>
+                                    </h3>
+                                </div>
+                                <Link to="/menu/services" className='services'>
+                                    <span>Got to services</span>
+                                    <HiArrowSmRight className='icon-button'></HiArrowSmRight>
+                                </Link>
+                            </article>
+                            <article className="tile">
+                                <div className="tile-header">
+                                    <TbPigMoney></TbPigMoney>
+                                    <h3>
+                                        <span>Exchange</span>
+                                    </h3>
+                                </div>
+                                <Link to="/menu/exchange" className='services'>
+                                    <span>Got to exchanges</span>
+                                    <HiArrowSmRight className='icon-button'></HiArrowSmRight>
+                                </Link>
+                            </article>
+                        </div>
+                        <div className="service-section-footer">
+                            <p>Services are paid according to the current state of the currency and tariff</p>
+                        </div>
+                    </section>
+                    <section className="transfer-section">
+                        <div className="transfer-section-header">
+                            <h2>Latest transfers</h2>
+                            <div className='filter-options'>
+                            <span>Choose a transfer to see more details</span>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
         </div>
     )
 }
