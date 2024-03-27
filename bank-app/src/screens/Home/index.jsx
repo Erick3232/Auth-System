c92525be3bc4c0223d9d96ac4b647efc04a7c469
leonardolermen <leosamer.lermen@gmail.com>
2024-03-27 01:59:32 -0300

    start app development

---
+++ bank-app/src/screens/Home/index.jsx
@@ -0,0 +1,15 @@
+import { Text } from 'react-native'
+import React from 'react'
+import { Container, MainInfoContainer } from './styled'
+
+const Home = () => {
+  return (
+    <Container>
+        <MainInfoContainer>
+            <Text>Home</Text>
+        </MainInfoContainer>
+    </Container>
+  )
+}
+
+export default Home;
---
+++ bank-app/src/screens/Home/styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.View`
+flex: 1;
+background-color: white;
+`;
+
+export const MainInfoContainer = styled.View`
+min-height: 250px;
+background-color: #161616;
+border-bottom-left-radius: 30px;
+border-bottom-right-radius: 30px;
+padding: 20px 0;
+`;
