67a53971501a3a1c24f15b8becb91051c1af8f02
Erick Miyagi <erick_miyagi34231@hotmail.com>
2024-06-17 18:41:32 -0300

    fix(interface): remove frontend

--- bank-ui/src/account/register/index.jsx
+++
@@ -1,9 +0,0 @@
-import './Register.css';
-import { RegisterInput } from "../../components/register/RegisterInput"
-export const Register = () => {
-    return (
-        <div className='register-body'>
-            <RegisterInput/>
-        </div>
-    );
-};
--- system/src/main/java/com/springauth/system/services/user/UserService.java
+++ system/src/main/java/com/springauth/system/services/user/UserService.java
@@ -13,59 +13,59 @@ import com.springauth.system.repositories.UserRepository;
 
 @Service
 public class UserService {
-    
+
     @Autowired
     private UserRepository userRepository;
 
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         return this.userRepository.findAll();
     }
 
-    public User findById(String userId){
+    public User findById(String userId) {
         Optional<User> obj = userRepository.findById(userId);
         return obj.orElseThrow(() -> new ResourceNotFoundException(userId));
     }
 
-    public boolean findByDocument(String document, String rg, String email){
-        User findedUser = userRepository.findByDocument(document,rg,email);
-        if(findedUser == null){
-        return true;
+    public boolean findByDocument(String document, String rg, String email) {
+        User findedUser = userRepository.findByDocument(document, rg, email);
+        if (findedUser == null) {
+            return true;
         } else
-        return false;
+            return false;
     }
 
-    public User findIdByLogin(String login){
+    public User findIdByLogin(String login) {
         User user = userRepository.findIdByLogin(login);
         return user;
     }
 
-    public User createUser(UserDTO data){
+    public User createUser(UserDTO data) {
         User newUser = new User(data);
         this.saveUser(newUser);
         return newUser;
     }
 
-    public User registerUser(RegisterDTO data){  
-            String encodedPassword = new BCryptPasswordEncoder().encode(data.password());
-            User newUser = new User(data, encodedPassword);
-            this.saveUser(newUser);
-            return newUser;  
+    public User registerUser(RegisterDTO data) {
+        String encodedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User newUser = new User(data, encodedPassword);
+        this.saveUser(newUser);
+        return newUser;
     }
 
-    public void saveUser(User user){
+    public void saveUser(User user) {
         userRepository.save(user);
     }
-    
-    public void delete(String id){
+
+    public void delete(String id) {
         findById(id);
         userRepository.deleteById(id);
     }
 
-    public User updateData(UpdateRequestService updateRequestService,String id){
+    public User updateData(UpdateRequestService updateRequestService, String id) {
         User newUser = findById(id);
         newUser.setEmail(updateRequestService.getEmail());
         newUser.setPassword(updateRequestService.getPassword());
         return userRepository.save(newUser);
     }
-    
+
 }
